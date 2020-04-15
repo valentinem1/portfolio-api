2a11218ad4ac8b3cab599ac401a3c49d88df7579
Valentine <valentinemaillard1@gmail.com>
2020-04-15 14:14:44 -0400

    fixed image issue in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,26 @@ Project.destroy_all
 Joiner.destroy_all
 Technology.destroy_all
 
-
 # Projects
 watif = Project.create(
     name: "Watif",
     description: "An e-marketplace modeled after Etsy - allowing creativity and uniqueness in products from handmade to vintage items and craft supplies.",
-    image: "./portfolio_images/watif.png",
+    image: "https://i.postimg.cc/W3tJMTDb/watif.png",    
     github: "https://github.com/valentinem1/watif_frontend",
     demo: "https://www.youtube.com/watch?v=DtVUKqseap8",
     live_website: ""
-)
+) 
+
+# Rails.root.join('app', 'assets', 'images', 'logo.png')
+# watif.image = Pathname.new(Rails.root.join("./db/portfolio_images/watif.png")).open
+# watif.save!
+# restaurant.logo = Pathname.new(Rails.root.join("db/images/mcdonalds_logo.png")).open
+# restaurant.save!
 
 travel_advisor = Project.create(
     name: "Travel Advisor",
     description: "A rating traveling app modeled after Trip Advisor - that offers a large selection of destinations throughout the world.",
-    image: "./portfolio_images/travel_advisor.png",
+    image: "https://i.postimg.cc/VNw6FM6N/travel-advisor.png",
     github: "https://github.com/valentinem1/Travel_Advisor_frontend",
     demo: "https://www.youtube.com/watch?v=1ZLSS1F6WBs&t=1s",
     live_website: "https://travel-advisor-app.herokuapp.com/"
@@ -25,7 +30,7 @@ travel_advisor = Project.create(
 planet_express = Project.create(
     name: "Planet Express",
     description: "A traveling app that allows people to fly to other planets - more than 10 flights per planet with real-time traveling data.",
-    image: "./portfolio_images/planet_expres.png",
+    image: "https://i.postimg.cc/kgBZhh21/planet-express.png",
     github: "https://github.com/valentinem1/Planet-Express-Frontend",
     demo: "https://www.youtube.com/watch?v=oP7dmeB0Y3E",
     live_website: ""
@@ -34,47 +39,47 @@ planet_express = Project.create(
 # Technologies
 react = Technology.create(
     name: "React",
-    image: "./portfolio_images/react-logo.png"
+    image: "portfolio_images/react-logo.png"
 )
 
 redux = Technology.create(
     name: "Redux",
-    image: "./portfolio_images/redux-logo.png"
+    image: "portfolio_images/redux-logo.png"
 )
 
 javascript = Technology.create(
     name: "JavaScript",
-    image: "./portfolio_images/javascript-logo.png"
+    image: "portfolio_images/javascript-logo.png"
 )
 
 ruby_on_rails = Technology.create(
     name: "Ruby on Rails",
-    image: "./portfolio_images/ruby-on-rails-logo.png"
+    image: "portfolio_images/ruby-on-rails-logo.png"
 )
 
 postgresql = Technology.create(
     name: "PostgreSQL",
-    image: "./portfolio_images/postgresql-logo.png"
+    image: "portfolio_images/postgresql-logo.png"
 )
 
 css = Technology.create(
     name: "CSS3",
-    image: "./portfolio_images/css-logo.png"
+    image: "portfolio_images/css-logo.png"
 )
 
 semantic_ui = Technology.create(
     name: "Semantic UI",
-    image: "./portfolio_images/semantic-ui-logo.png"
+    image: "portfolio_images/semantic-ui-logo.png"
 )
 
 stripe_api = Technology.create(
     name: "Stripe API",
-    image: "./portfolio_images/stripe-api-logo.jpg"
+    image: "portfolio_images/stripe-api-logo.jpg"
 )
 
 esty_api = Technology.create(
     name: "Etsy API",
-    image: "./portfolio_images/etsy-logo.png"
+    image: "portfolio_images/etsy-logo.png"
 )
 
 # Joiners
